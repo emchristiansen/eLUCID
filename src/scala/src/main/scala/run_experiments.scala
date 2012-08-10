5cbdae314211d361e5e457c52fe59963e02b0f8f
Eric Christiansen <ericmartinchristiansen@gmail.com>
2012-08-10 10:16:55 -0700

    can now run simple experiments

--- src/scala/src/main/scala/run_experiments.scala
+++ src/scala/src/main/scala/run_experiments.scala
@@ -8,23 +8,18 @@ import util.Random
 
 import com.frugalmechanic.optparse._
 
-object Global {
-  val homeDirectory = System.getProperty("user.home")
-  var runVar: Option[RuntimeConfig] = None
-  def run: RuntimeConfig = runVar match {
-    case Some(config) => config
-    case None => throw new Exception("Global.run not initialized")
-  }
-}
+import nebula._
 
 object RunExperiments extends OptParse {
-  def runExperiment(experiment: Experiment) { 
-    if (Global.run.skipCompletedExperiments && experiment.alreadyRun) { 
+  def runExperiment(experiment: CorrespondenceExperiment) { 
+    if (Global.run[RuntimeConfig].skipCompletedExperiments && experiment.alreadyRun) { 
       println("Already run, skipping: %s".format(experiment))
       return
     } else {
       println("Running experiment: %s".format(experiment))
     }
+
+    println(experiment.leftImageFile)
 
     val leftImage = experiment.leftImage
     val rightImage = experiment.rightImage
@@ -32,16 +27,16 @@ object RunExperiments extends OptParse {
     val (leftKeyPoints, rightKeyPoints) = {
       val leftKeyPoints = experiment.detector(leftImage)
       Util.pruneKeyPoints(leftImage, 
-			  rightImage, 
-			  experiment.homography, 
-			  leftKeyPoints).unzip
+  			  rightImage, 
+  			  experiment.homography, 
+  			  leftKeyPoints).unzip
     }
 
     println("Number of KeyPoints: %s".format(leftKeyPoints.size))
 
     val (leftDescriptors, rightDescriptors) = {
-      val leftDescriptors = experiment.extractor(leftKeyPoints, leftImage)
-      val rightDescriptors = experiment.extractor(rightKeyPoints, rightImage)
+      val leftDescriptors = experiment.extractor(leftImage, leftKeyPoints)
+      val rightDescriptors = experiment.extractor(rightImage, rightKeyPoints)
 
       for ((Some(left), Some(right)) <- leftDescriptors.zip(rightDescriptors)) yield (left, right)
     } unzip
@@ -50,31 +45,40 @@ object RunExperiments extends OptParse {
 
     val dmatches = experiment.matcher(true, leftDescriptors, rightDescriptors)
 
-    // TODO: save ExperimentResults and print recognition rate
+    val results = CorrespondenceExperimentResults(experiment, dmatches)
+    results.save
+
+    println("Recognition rate was %s".format(Summary.recognitionRate(results.dmatches)))
   }
 
   val runtimeConfigFile = StrOpt()
   val experimentConfigFilesOrDirectory = MultiStrOpt()
+
   def main(args: Array[String]) { 
     parse(args)
 
-    val runtimeConfig = RuntimeConfig.fromFile(new File(runtimeConfigFile.get))
+    val runtimeConfig = IO.fromJSONFile[RuntimeConfig](new File(runtimeConfigFile.get))
     Global.runVar = Some(runtimeConfig)
+
+    println(runtimeConfig)
 
     val experimentConfigFiles: List[File] = {
       val paths = experimentConfigFilesOrDirectory.get
       val files = paths.map(path => new File(path)).toList
       files match {
-	case List(file) => if (file.isDirectory) file.listFiles.toList
-			   else files
-	case _ => files
+    	case List(file) => if (file.isDirectory) file.listFiles.toList
+    			   else files
+    	case _ => files
       }
     }
 
+    val file = Global.homeDirectory + "/" + Global.run[RuntimeConfig].projectRoot + "/config/experiments/debug.json"
+
     if (experimentConfigFiles.size == 0) println("No experiments specified.")
-    
-    val experimentConfigs = experimentConfigFiles.map(ExperimentConfig.fromFile)
-    val experiments = experimentConfigs.flatMap(Experiment.fromConfig)
+
+    val experimentConfigs = experimentConfigFiles.map(CorrespondenceExperimentConfig.fromJSONFile)
+
+    val experiments = experimentConfigs.flatMap(CorrespondenceExperiment.fromConfig)
 
     println("There are %s experiments:".format(experiments.size))
     experiments.foreach(println)
